78a107e2d3c43866dc0c35b430929c9f8f190df9
kjunn2000 <kjunn2000@gmail.com>
2022-03-09 21:55:51 +0800

    Implement Authentication Module

---
+++ NewLifeThriftShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NewLifeThriftShop.Areas.Identity.Data;
+using NewLifeThriftShop.Data;
+
+[assembly: HostingStartup(typeof(NewLifeThriftShop.Areas.Identity.IdentityHostingStartup))]
+namespace NewLifeThriftShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<NewLifeThriftShopContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("NewLifeThriftShopContextConnection")));
+
+                services.AddDefaultIdentity<NewLifeThriftShopUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<NewLifeThriftShopContext>();
+            });
+        }
+    }
+}
